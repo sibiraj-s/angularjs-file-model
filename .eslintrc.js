4ba0481bbb18dbc34638dc0ea05f0eb78a5564b5
Sibiraj <dev.sibiraj@outlook.com>
2018-08-27 15:11:57 +0530

    style: use two space indentation instead of four

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    'operator-linebreak': ['error', 'before'],
+    'padded-blocks': ['error', 'never']
+  }
+};
